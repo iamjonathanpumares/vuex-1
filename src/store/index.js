1b71593ef0318e7dd4b10ca767bae9831d993794
Jonathan Pumares <jepc1491@gmail.com>
2021-01-04 19:50:52 -0600

    Práctica de un contador del curso de VueJS utilizando Vuex.

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador: 150
   },
   mutations: {
+    incrementar(state, payload) {
+      state.contador = state.contador + payload
+    },
+    disminuir(state, payload) {
+      state.contador = state.contador - payload
+    }
   },
   actions: {
+    accionIncrementar({ commit }) {
+      commit('incrementar', 10)
+    },
+    accionDisminuir({ commit }, numero) {
+      commit('disminuir', numero)
+    },
+    accionBoton({ commit }, item) {
+      if (item.estado) {
+        commit('incrementar', item.numero)
+      }
+      else {
+        commit('disminuir', item.numero)
+      }
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1
+      :style="colorContador"
+    >
+      {{ titulo }}: {{ contador }}
+    </h1>
+    <button @click="accionIncrementar">Aumentar</button>
+    <BotonDisminuir/>
+
+    <hr>
+
+    <BotonAccion :estado="true"/>
+    <BotonAccion :estado="false"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BotonDisminuir from '../components/BotonDisminuir'
+import BotonAccion from '../components/BotonAccion'
+import { mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    BotonDisminuir,
+    BotonAccion
+  },
+  data() {
+    return {
+      titulo: 'Mi contador Vuex'
+    }
+  },
+  computed: {
+    ...mapState(['contador']),
+    colorContador() {
+      return [this.contador > 100 ? {'color': 'green'} : {'color': 'red'}]
+    }
+  },
+  methods: {
+    ...mapMutations(['incrementar']),
+    ...mapActions(['accionIncrementar'])
   }
 }
 </script>
